3dd71cf7fb03088c52045c374db7bdece8edc719
Ioan Radu <ioanradu_2004@yahoo.com>
2019-05-22 13:03:14 +0300

    improved extrage method and the contruct without parameters

--- src/inheritancePolymorphismAbstraction/exercitiul1/Main.java
+++ src/inheritancePolymorphismAbstraction/exercitiul1/Main.java
@@ -2,8 +2,7 @@ package inheritancePolymorphismAbstraction.exercitiul1;
 
 public class Main {
     public static void main(String[] args) {
-        Multime m = new Multime(6);
-        System.out.println(m.toString());
+        Multime m = new Multime(7);
         m.afisare();
         m.adauga(4);
         m.afisare();
@@ -24,13 +23,11 @@ public class Main {
         m.adauga(5);
         m.afisare();
         m.adauga(10);
+        m.extrage(2);
         m.afisare();
         m.adauga(11);
         m.afisare();
         m.adauga(12);
         m.afisare();
-
-
     }
-
 }
--- src/inheritancePolymorphismAbstraction/exercitiul1/Multime.java
+++ src/inheritancePolymorphismAbstraction/exercitiul1/Multime.java
@@ -7,11 +7,13 @@ public class Multime {
 
     public Multime() {
         int n = 0;
+        this.dim = 10; // dimensiunea default al array-ului daca se instantiaza obiectul fara a se declara si dimensiunea array-ului
+        this.date = new int[dim];
     }
 
     public Multime(int dim) {
         this();
-        //setez dimensiunea array-ul odata cu instantierea obiectului din metoda Main().
+        //setez dimensiunea array-ul odata cu instantierea obiectului din metoda main().
         this.dim = dim;
         this.date = new int[dim];
     }
@@ -21,7 +23,7 @@ public class Multime {
             System.out.println("Array is full!");
         } else {
             boolean writeOnce = false;
-            for (int i = 0; i < dim; i++) {
+            for (int i = 0; i < date.length; i++) {
                 if (date[i] == number) {
                     break;
                 } else if (!writeOnce && date[i] == 0) {
@@ -34,19 +36,25 @@ public class Multime {
     }
 
     public void extrage(int number) {
+        boolean find = false;
         for (int i = 0; i < date.length; i++) {
-            if (date[i] != number) {
-                continue;
-            } else {
-                date[i] = 0;
-                n--;
+            if (date[i] == number) {
+                int index = i; // salvez in variabila index index-ul elementului pe care vreau sa-l sterg din array
+                find = true;
+                for (i = index; i < date.length - 1; i++) {
+                    date[i] = date[i + 1]; // shiftez elementele array-ului spre stanga incepand de la indexul elementului pe care vreau sa-l sterg
+                }
             }
+        }
+        date[date.length - 1] = 0;
+        if (find) {
+            n--;
         }
     }
 
     public void afisare() {
         for (int i = 0; i < date.length; i++) {
-            System.out.print(date[i] + " ");
+            System.out.printf("%2d ", date[i]);
         }
         System.out.println();
     }
